99d6b2c3875d215c62e50b2046656dacf4dcbf6e
Пользователь <zilola.ikramova96@gmail.com>
2022-10-30 01:36:35 -0900

    gallary and sidebar done

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const modal = document.getElementById("myModal");
 const btn = document.getElementById("myBtn");
 const span = document.getElementsByClassName("close")[0];
 const sidebarClose = document.getElementById("sidebarClose");
-const wrapper = document.getElementById("wrapper");
 
 
 
@@ -11,13 +10,22 @@ btn.onclick = function () {
     modal.style.display = "block";
     modal.style.width="100%"
 }
+// btn2.onclick = function () {
+//     modal.style.display = "block";
+// }
+// btn3.onclick = function () {
+//     modal.style.display = "block";
+// }
+
+
 span.onclick = function () {
     // modal.style.display = "none";
     modal.style.width="0%"
     $("#wrapper").style.width="0%"
-    $("#wrapper").removeClass('toggled');
 }
-
+// span2.onclick = function () {
+//     modal2.style.display = "none";
+// }
 window.onclick = function (event) {
     if (event.target == modal) {
         // modal.style.display = "none";
@@ -25,6 +33,7 @@ window.onclick = function (event) {
                 // modal2.style.display = "none";
     }
 }
+
 
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
@@ -58,23 +67,58 @@ $(document).ready(function(){
     });
   });
   
-// $("#menu-toggle").click(function(e) {
+  
+  /* CANVAS JS */
+  function progressSim(selector) {
+    $(selector).each(function() {
+      const $item = $(this);
+  
+      // prevent double initializetion of canvas
+      if ($item.data('initilized') === true) {
+        return;
+      }
+  
+      const percent = $item.data('percent');
+  
+      let ctx = $item.get(0).getContext('2d'),
+          cw = ctx.canvas.width,
+          ch = ctx.canvas.height,
+          al = 0,
+          sim = setInterval(progress, 25);
+  
+      // mark already initialized canvases to prevent double init
+      $item.data('initilized', true);
+  
+      function progress() {
+        let start = 4.72,
+            diff = ((al / 100) * Math.PI * 2 * 10).toFixed(2);
+  
+        ctx.clearRect(0, 0, cw, ch);
+        ctx.lineWidth = 7;
+        ctx.fillStyle = '#000';
+        ctx.strokeStyle = "#000";
+        ctx.textAlign = 'center';
+        ctx.font="30px Arial";
+        ctx.fillText(al + '%', cw * .5, ch * .5 + 2, cw);
+        ctx.beginPath();
+        ctx.arc(80, 80, 70, start, diff / 10 + start, false);
+        ctx.stroke();
+        if (al >= percent) {
+          clearTimeout(sim);
+        }
+        al++;
+      };
+    })        
+  }
+  function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+  }
+//   $("#menu-toggle").click(function(e) {
 //     e.preventDefault();
-//     $("#wrapper").addClass("toggled");   
+//     $("#wrapper").toggleClass("toggled"); 
 // });
 
-// $("#sidebarClose").click(function(e) {
-//   $("#wrapper").removeClass("toggled");   
-
- 
-// });
-
-function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-  // document.getElementById("main").style.marginLeft = "250px";
-}
-
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  // document.getElementById("main").style.marginLeft= "0";
-}
